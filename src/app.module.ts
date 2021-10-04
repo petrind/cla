e855993d0e62fafd89e4ab7c035856eefea5cb2c
petrus <petr_ind@yahoo.com>
2021-10-04 20:44:57 +0700

    feat(modules): remove unneeded module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,18 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ContactListModule } from './modules/domain/contact-list/contact-list.module';
+import { ContactModule } from './modules/domain/contact/contact.module';
+import { UserModule } from './modules/domain/user/user.module';
+import * as config from 'config';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    TypeOrmModule.forRoot(config.get('db')),
+    ContactListModule,
+    ContactModule,
+    UserModule,
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
